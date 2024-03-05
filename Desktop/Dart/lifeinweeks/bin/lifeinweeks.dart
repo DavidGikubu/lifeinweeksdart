87e375c3d57dd664a9d97649fdb39fad319e7097
Gikubu <davidndungu8203@gmail.com>
2024-03-05 13:26:18 +0300

    here all the necessary files are added to the project

---
+++ Desktop/Dart/lifeinweeks/bin/lifeinweeks.dart
@@ -0,0 +1,5 @@
+import 'package:lifeinweeks/lifeinweeks.dart' as lifeinweeks;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lifeinweeks.calculate()}!');
+}
---
+++ Desktop/Dart/lifeinweeks/test/lifeinweeks_test.dart
@@ -0,0 +1,8 @@
+import 'package:lifeinweeks/lifeinweeks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
