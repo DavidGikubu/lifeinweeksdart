c1f5edddbd103de2c99e8a14923401151737856e
Gikubu <davidndungu8203@gmail.com>
2024-03-05 16:01:40 +0300

    Revert "here all the necessary files are added to the project"
    This reverts commit 87e375c3d57dd664a9d97649fdb39fad319e7097.

--- Desktop/Dart/lifeinweeks/bin/lifeinweeks.dart
+++
@@ -1,5 +0,0 @@
-import 'package:lifeinweeks/lifeinweeks.dart' as lifeinweeks;
-
-void main(List<String> arguments) {
-  print('Hello world: ${lifeinweeks.calculate()}!');
-}
--- Desktop/Dart/lifeinweeks/test/lifeinweeks_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lifeinweeks/lifeinweeks.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
